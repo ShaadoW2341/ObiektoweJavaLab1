4d54170767fe7b95a0d3d7edb15961fdd2d7aeac
Krzysztof Adamski <153490135+ShaadoW2341@users.noreply.github.com>
2024-05-20 00:28:55 +0200

    Delete lab_08 directory

--- lab_08/src/BlednaWartoscDlaSilniException.java
+++
@@ -1,6 +0,0 @@
-//do zadania drugiego
-public class BlednaWartoscDlaSilniException extends Exception {
-    public BlednaWartoscDlaSilniException(String message) {
-        super(message);
-    }
-}
--- lab_08/src/NieprawidlowyAdresException.java
+++
@@ -1,6 +0,0 @@
-//do zadania trzeciego
-public class NieprawidlowyAdresException extends Exception{
-    public NieprawidlowyAdresException(String message) {
-        super(message);
-    }
-}
